9b44555f45bceae9139de511ffc255cd55c3677c
ernestoneyra <ernestoneyra31@gmail.com>
2021-01-30 16:35:24 +0100

    first push

---
+++ Inlämningsuppgift2/excercise2/src/Scoreblock/Scoreblock.jsx
@@ -0,0 +1,18 @@
+const styles = {
+  border: "3px solid black",
+  margin: "5px",
+};
+
+const Scoreblock = ({ score }) => {
+  return (
+    <div className="mb-3 p-3" style={styles}>
+      <h4>{score.game}</h4>
+      <div>
+        {score.player}, {score.date}
+        <p className="float-right">{score.highscore}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Scoreblock;
