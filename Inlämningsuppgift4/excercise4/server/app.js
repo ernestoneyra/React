4062a9b2e04a05ecd581f6c1ac8ee909374818a4
Ernesto Neyra <ernestoneyra31@gmail.com>
2021-04-11 19:42:01 +0200

    post works with postman, not vs code

--- Inlämningsuppgift4/excercise4/server/app.js
+++ Inlämningsuppgift4/excercise4/server/app.js
@@ -1,53 +1,118 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const app = express();
+const port = 5000;
 var cors = require('cors')
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
- testapiRouter = require('./routes/testapi');
- scoresRouter = require('./routes/scores');
- gamesRouter = require('./routes/games');
-
-var bodyParser = require('body-parser');
-/* app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies */
 
 
-var app = express();
+const bodyParser = require("body-parser")
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors())
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testapi', testapiRouter);
-app.use('/scores', scoresRouter);
-app.use('/games', gamesRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.post("/scores",(req,res) => {
+   /* res = {
+    game: req.body.game,
+    player: req.body.player,
+    date: req.body.date,
+    highscore: req.body.highscore,
+    urlSlug: req.body.urlSlug
+   
+}  */
+const score = req.body;
+scores.push(score);
+  //code to perform particular action.
+  //To access POST variable use req.body()methods.
+  console.log(scores)
+  res.json(scores)
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+let games = [
+  {
+    id: 1,
+    game: "Tetris",
+    genre: "Genre: Puzzle",
+    urlSlug: "tetris",
+    release_year: "Release Year: 1984",
+    description:
+      "Allows players to rotate falling blocks strategically to clear levels. ",
+    imageUrl:
+      "https://assets.partyking.org/img/products/1300/tetris-lampa-1.jpg",
+  },
+  {
+    id: 2,
+    game: "Pacman",
+    genre: "Genre: Platform",
+    urlSlug: "pacman",
+    release_year: "Release Year: 1980",
+    description:
+      "The player controls Pac-Man, who must eat all the dots inside an enclosed maze while avoiding four colored ghosts. Eating large flashing dots called Power Pellets causes the ghosts to turn blue, allowing Pac-Man to eat them for bonus points.",
+    imageUrl: "https://thumbs.dreamstime.com/z/pacman-symboler-128515491.jpg",
+  },
+  {
+    id: 3,
+    game: "Asteroids",
+    urlSlug: "asteroids",
+    genre: "Genre: Shooter",
+    release_year: "Relase Year: 1979",
+    description:
+      "The object of the game is to shoot and destroy the asteroids and saucers, while not colliding with either, or being hit by the saucers' counter-fire.",
+    imageUrl:
+      "https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_350/MTc0MDExMjc3NTI4NTQxMDUx/asteroids-game.webp",
+  },
+];
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+let scores = [
+  {
+    id: 1,
+    game: {
+      id: 1,
+      title: "Tetris",
+    },
+    player: "John Doe",
+    date: "2019-01-01",
+    highscore: "130 000",
+    urlSlug: "tetris",
+  },
+  {
+    id: 2,
+    game: {
+      id: 2,
+      title: "Pacman",
+    },
+    player: "Jane Doe",
+    date: "2019-02-02 ",
+    highscore: "120 000",
+    urlSlug: "pacman",
+  },
+  {
+    id: 3,
+    game: {
+      id: 3,
+      title: "Asteroids",
+    },
+    player: "Jan Doe",
+    date: "2019-03-03",
+    highscore: "200 000",
+    urlSlug: "asteroids",
+  },
+];
+
+app.get("/games", function (req, res, next) {
+ 
+  res.json(games);
 });
 
-module.exports = app;
+app.get("/scores", function (req, res, next) {
+  
+  res.json(scores);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
